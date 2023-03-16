0f04555150b06c2e7e42c3c2a5344a1861bcf2e5
Yashasvi <66861659+stlyash@users.noreply.github.com>
2023-03-16 07:17:11 +0530

    Delete try.py

--- try.py
+++
@@ -1,6 +0,0 @@
-import re
-
-textString = '''Good muffins cost $3.88\nin New York. I am we and I live in USA Please buy me two of them.\n\nThanks.'''
-forPronounCount = re.compile(r'\b(I|we|ours|my|mine|(?-i:us))\b', re.I)
-pronCount = len(forPronounCount.findall(textString))
-print(pronouns)
